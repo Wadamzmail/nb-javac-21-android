bf46d8b7365b3a952b59fedf6857909a9fdf9a40
Wadamzmail <alksndrmetwakil@gmail.com>
2026-09-23 10:01:45 +0200

    fix: ci

--- setup_base.sh
+++ setup_base.sh
@@ -5,8 +5,7 @@ set -e
 export ant_dir=./make/langtools/netbeans/nb-javac
 
 # Build nb-javac-android
-JDK_8="/usr/lib/jvm/java-21-openjdk"
-
+JDK_8="/data/data/com.termux/files/usr/lib/jvm/java-21-openjdk"
 if [[ ! -z "$CI" ]]; then
   JDK_8=$JAVA_8
   echo "langtools.jdk.home=${JAVA_HOME}" >> make/langtools/build.properties
